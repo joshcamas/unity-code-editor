795366cc1cdf355ae10d838572dd3958a6ecaf7b
joshcamas <joshcamas@gmail.com>
2025-08-30 10:07:31 -0700

    iniital implementation

---
+++ CodeEditor/ExampleWindow.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using UnityEditor;
+
+namespace Joshcamas.CodeEditor
+{
+    public class ExampleWindow : EditorWindow
+    {
+        private EditorView view = new EditorView();
+
+        [MenuItem("Joshcamas/Code Editor Example")]
+        public static void OpenWindow()
+        {
+            CreateWindow<ExampleWindow>("Example Code Editor");
+        }
+        
+        private void OnEnable()
+        {
+            view.OnEnable(string.Empty);
+        }
+
+        private void OnGUI()
+        {
+            view.EditorViewGUI();
+        }
+    }
+}
